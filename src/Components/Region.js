7b853c3fc8ad9eff7d41d34ae2522fdf00078429
RSR <rajiridwanullah25@gmail.com>
2022-09-19 21:01:55 +0100

    a

--- src/Components/Region.js
+++ src/Components/Region.js
@@ -22,7 +22,7 @@ const Region = () => {
     <div>
      <Search />
       <div className="grid md:grid-cols-3 gap-12 px-3 md:px-8 dark:bg-[#212E37]">
-     { isLoading ? details.map((detail,index) => <Card key={index} detail={ detail } id={ detail.name } />) : <div className='w-screen mt-10 font-light text-cenmx-auto'>Loading....</div> }
+     { isLoading ? details.map((detail,index) => <Card key={index} detail={ detail } id={ detail.name } />) : <div className='w-screen mt-10 font-light text-center mx-auto'>Loading....</div> }
          </div>
       </div>
     )
